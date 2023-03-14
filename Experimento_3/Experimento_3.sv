53152c5dde7359147a1ea9334ed28e9cd998de3b
Johnny Aguero <johnny.zaet08@gmail.com>
2023-03-13 18:02:35 -0600

    add a testbench of the experiment 3

--- Experimento_3/Experimento_3.sv
+++ Experimento_3/Experimento_3.sv
@@ -1,19 +1,18 @@
 module Experimento_3 #( parameter N = 6 ) (
 	input logic clk, 
 	input logic reset,
-	output logic [6:0] led1, 
-	output logic [6:0] led2
+	output logic [6:0] display1, 
+	output logic [6:0] display2
 );
 
 	
 
 	reg[N-1:0] contador;
-	
-	convertidor_a_display convertidor1(contador[3], contador[2], contador[1], contador[0], led1);
-	convertidor_a_display convertidor2(1'b0, 1'b0, contador[5], contador[4], led2);
-	
-	
 	reg[N-1:0] contador_temp = (2**N) - 1;
+	
+	convertidor_a_display convertidor1(contador[3], contador[2], contador[1], contador[0], display1);
+	convertidor_a_display convertidor2(1'b0, 1'b0, contador[5], contador[4], display2);
+		
 	
 	always @(posedge clk or posedge reset)
 		begin
