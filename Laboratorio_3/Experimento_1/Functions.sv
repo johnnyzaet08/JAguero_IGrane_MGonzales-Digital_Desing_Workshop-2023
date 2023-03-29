220114b0e1d9745ce9be8f4131824d73185eca56
Quigonar <67380073+Quigonar@users.noreply.github.com>
2023-03-28 23:44:09 -0600

    Added flags implementation
    added implementation for flags in the functions file

--- Laboratorio_3/Experimento_1/Functions.sv
+++ Laboratorio_3/Experimento_1/Functions.sv
@@ -47,11 +47,53 @@ endmodule
 module divider #( parameter N ) 
 	(
 		input [N-1:0] A, B, 
-		output [N-1:0] Q, R 
+		output [N-1:0] Q, R,
+		output logic zero
 	);
+	
+	reg [N-1:0] dividend;
+	reg [N-1:0] divisor;
+	reg [N-1:0] quotient;
+	reg [N-1:0] remainder;
+	reg [N-1:0] shifted_divisor;
+	reg [N:0]   count;
+    
+	assign Q = quotient;
+	assign R = remainder;
+    
+	always @(*) begin
+		dividend = A;
+		divisor = B;
+		quotient = 0;
+		remainder = 0;
+		count = N+1;
+        
+		// Shift divisor to align with dividend
+		shifted_divisor = divisor << N;
+		if (divisor == 0) begin
+			zero = 1;
+		end else begin
+			zero = 0;
+         while (count > 0) begin
+				count = count - 1;
+            remainder = remainder << 1;
+            remainder[0] = dividend[N-1];
+            dividend = dividend << 1;
+            
+            if (remainder >= shifted_divisor) begin
+					remainder = remainder - shifted_divisor;
+               quotient[count] = 1;
+				end
+			end
+		end
+	end
 
 endmodule
 
+<<<<<<< Updated upstream
+=======
+/*
+>>>>>>> Stashed changes
 // N-bit MOD module
 module MOD #( parameter N )
 	(
@@ -65,6 +107,7 @@ module MOD #( parameter N )
 	assign ext_a = {{DATA_WIDTH{a[DATA_WIDTH-1]}}, a};
 	assign quotient = ext_a / b;
 	assign out = ext_a - (quotient * b);
+<<<<<<< Updated upstream
 	
 endmodule
 
@@ -81,14 +124,41 @@ module full-adder #( parameter N )
 	
 endmodule
 
+=======
+	
+endmodule */
+
+// N-bit full adder module
+module full_adder #( parameter N ) 
+	(
+		input [N-1:0] A, B,
+		input logic c_in,
+		output [N-1:0] sum, 
+		output logic c_out
+	);
+	
+	assign {c_out, sum} = A + B + c_in;
+	
+endmodule
+
+>>>>>>> Stashed changes
 // N-bit substractor module
 module substractor #( parameter N )
 	(
 		input [N-1:0] A, B,
+<<<<<<< Updated upstream
 		output [N-1:0] sub
 	);
 	
 	assign sub = A - B;
+=======
+		output [N-1:0] sub,
+		output logic negative
+	);
+	
+	assign sub = A - B;
+	assign negative = (sub < 0);
+>>>>>>> Stashed changes
 	
 endmodule
 
@@ -96,9 +166,27 @@ endmodule
 module multiplier #( parameter N )
 	(
 		input [N-1:0] A, B,
+<<<<<<< Updated upstream
 		output [2*N-1:0] mult
+=======
+		output [2*N-1:0] mult,
+		output logic overflow
+>>>>>>> Stashed changes
 	);
 	
 	assign mult = A * B;
 	
+<<<<<<< Updated upstream
+=======
+	always @(*) begin
+		localparam max_val = (1 << (2*N)) - 1; // maximum value that can be represented by 2*N bits
+		
+		if (mult > max_val) begin
+			assign overflow = 1;
+		end else begin
+			assign overflow = 0;
+		end
+	end
+	
+>>>>>>> Stashed changes
 endmodule
