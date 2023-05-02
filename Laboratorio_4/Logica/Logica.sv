7f36af8e6585e9ea26a4d5f61e13a956c5a44f71
Quigonar <67380073+Quigonar@users.noreply.github.com>
2023-05-01 22:03:34 -0600

    cambio logica

--- Laboratorio_4/Logica/Logica.sv
+++ Laboratorio_4/Logica/Logica.sv
@@ -1,4 +1,4 @@
-module Logica(input int matrixIn[3:0][3:0], input logic [3:0] movement, input clk, rst, output int matrixOut[3:0][3:0]);
+module Logica(input logic [3:0] movement, input clk, rst);
 
 	// parametros locales
 	localparam 
@@ -12,7 +12,8 @@ module Logica(input int matrixIn[3:0][3:0], input logic [3:0] movement, input cl
 	
 	// asignar a la matriz donde se va a trabajar la matriz de entrada
 	int matrixWork[3:0][3:0];
-	assign matrixWork = matrixIn;
+	
+	wire check;
 	
 	// estados de la maquina de estados
 	localparam
@@ -21,8 +22,7 @@ module Logica(input int matrixIn[3:0][3:0], input logic [3:0] movement, input cl
 	WIN = 3'b011, // ganar
 	MOVE = 3'b100, // mover hacia un lado
 	MERGE = 3'b101, // combinar celdas
-	END = 3'b110, // fin del juego
-	CONTINUE = 3'b111; // continua el juego
+	CONTINUE = 3'b110; // continua el juego
 	
 	// logica para chequear y sumar blockes andjuntos
 	task hacer_sumas(input int matrixVer [3:0][3:0], input logic [3:0] movement,output int matrixSum [3:0][3:0]);
@@ -32,7 +32,7 @@ module Logica(input int matrixIn[3:0][3:0], input logic [3:0] movement, input cl
 				// para revisar hacia la derecha
 				for (int i = 0; i < 4; i++) begin
 					for (int j = 3; j >= 0; j--) begin
-						if (j < 3) begin 
+						if (j < 3 && matrixSum[i][j] != 0) begin 
 							if (matrixSum[i][j] == matrixSum[i][j+1]) begin
 								matrixSum[i][j+1] *= 2;
 								matrixSum[i][j] = 0;
@@ -45,7 +45,7 @@ module Logica(input int matrixIn[3:0][3:0], input logic [3:0] movement, input cl
 				// para revisar hacia la izquierda
 				for (int i = 0; i < 4; i++) begin
 					for (int j = 0; j < 4; j++) begin
-						if (j > 0) begin 
+						if (j > 0 && matrixSum[i][j] != 0) begin 
 							if (matrixSum[i][j] == matrixSum[i][j-1]) begin
 								matrixSum[i][j-1] *= 2;
 								matrixSum[i][j] = 0;
@@ -58,7 +58,7 @@ module Logica(input int matrixIn[3:0][3:0], input logic [3:0] movement, input cl
 				// para revisar hacia arriba
 				for (int i = 0; i < 4; i++) begin
 					for (int j = 0; j < 4; j++) begin
-						if (i > 0) begin 
+						if (i > 0 && matrixSum[i][j] != 0) begin 
 							if (matrixSum[i][j] == matrixSum[i-1][j]) begin
 								matrixSum[i-1][j] *= 2;
 								matrixSum[i][j] = 0;
@@ -71,7 +71,7 @@ module Logica(input int matrixIn[3:0][3:0], input logic [3:0] movement, input cl
 				// para revisar hacia abajo
 				for (int i = 3; i >= 0; i--) begin
 					for (int j = 0; j < 4; j++) begin
-						if (i < 3) begin
+						if (i < 3 && matrixSum[i][j] != 0) begin
 							if (matrixSum[i][j] == matrixSum[i+1][j]) begin
 								matrixSum[i+1][j] *= 2;
 								matrixSum[i][j] = 0;
@@ -82,12 +82,70 @@ module Logica(input int matrixIn[3:0][3:0], input logic [3:0] movement, input cl
 			end
 		endcase
 	endtask
-				
+	
+	task mover_nums(input int matrixVer [3:0][3:0], input logic [3:0] movement, output int matrixMov [3:0][3:0]);
+		matrixMov = matrixVer;
+		case (movement)
+			4'b0001: begin
+				// para mover hacia la derecha, revisar logica porque se tendria que reiniciar despues de cada movimiento de casilla pero se encicla y no sale
+				for (int i = 0; i < 4; i++) begin
+					for (int j = 3; j >= 0; j--) begin
+						if (j < 3 && matrixMov[i][j] != 0) begin 
+							if (matrixMov[i][j+1] == 0) begin
+								matrixMov[i][j+1] = matrixMov[i][j];
+								matrixMov[i][j] = 0;
+								// j = 3;
+							end
+						end
+					end
+				end
+			end
+			4'b0010: begin
+				// para mover hacia la izquierda
+				for (int i = 0; i < 4; i++) begin
+					for (int j = 0; j < 4; j++) begin
+						if (j > 0 && matrixMov[i][j] != 0) begin 
+							if (matrixMov[i][j-1] == 0) begin
+								matrixMov[i][j-1] = matrixMov[i][j];
+								matrixMov[i][j] = 0;
+							end
+						end
+					end
+				end
+			end
+			4'b0100: begin
+				// para mover hacia arriba
+				for (int i = 0; i < 4; i++) begin
+					for (int j = 0; j < 4; j++) begin
+						if (i > 0 && matrixMov[i][j] != 0) begin 
+							if (matrixMov[i-1][j] == 0) begin
+								matrixMov[i-1][j] = matrixMov[i][j];
+								matrixMov[i][j] = 0;
+							end
+						end
+					end
+				end
+			end
+			4'b1000: begin
+				// para mover hacia abajo
+				for (int i = 3; i >= 0; i--) begin
+					for (int j = 0; j < 4; j++) begin
+						if (i < 3 && matrixMov[i][j] != 0) begin
+							if (matrixMov[i+1][j] == 0) begin
+								matrixMov[i+1][j] = matrixMov[i][j];
+								matrixMov[i][j] = 0;
+							end
+						end
+					end
+				end
+			end
+		endcase
+	endtask
 		
 	
-	/*always_ff @ (posedge clk or posedge rst)
-		if (rst) state = 3'b000;
-		else state = next_state;*/
+	always_ff @ (posedge clk or posedge rst)
+		if (rst) state = RESET;
+		else state = next_state;
 	
 	always @ (*)
 		begin
@@ -95,34 +153,35 @@ module Logica(input int matrixIn[3:0][3:0], input logic [3:0] movement, input cl
 				RESET: begin
 					matrixWork = '{ '{0,0,0,0}, '{0,0,0,0}, '{0,0,0,0}, '{0,0,0,0} };
 					// tambien se requiere hacer la parte de asignar un par de valores inciales aleatoriamente en el tablero
-					state <= CONTINUE;
+					next_state = CONTINUE;
 				end
 				CONTINUE: begin
-					//assign matrixOut = matrixWork; <----- Esto esta dando error!
-					if (WON||LOST) begin
-						state <= END;
+					//matrixOut = matrixWork;
+					if (LOST) begin
+						next_state = LOSE;
+					end else if (WON) begin
+						next_state = WIN;
 					end else begin
-						// asignar de manera aleatoria el nuevo valor en el tablero
-						state <= MOVE;
+						next_state = MOVE;
 					end
 				end
 				MOVE: begin
-					// chequear si hay bloques uno a la par del otro para hacerles el merge
-					state <= MERGE;
-					// si no state <= CONTINUE
+					// aqui se mueven los numeros en el tablero dependiendo del movimiento realizado
+					mover_nums(matrixWork, movement, matrixWork);
+					next_state = MERGE;
 				end 
 				MERGE: begin
 					// aqui se verifica el tablero y se hacen los merges necesarios
 					hacer_sumas(matrixWork, movement, matrixWork);
-					state <= CONTINUE;
+					next_state = CONTINUE;
 				end
-				END: begin
+				LOSE: begin
 					// logica para perder, es decir que no haya ningun espacio en 0
-					state <= RESET;
+					next_state = RESET;
 				end
 				WIN: begin
 					// logica para ganar, es decir que se haya llegado a WIN_CONDITION
-					state <= RESET;
+					next_state = RESET;
 				end
 			endcase
 		end
